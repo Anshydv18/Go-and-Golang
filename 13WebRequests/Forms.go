c30d79a3c131497131d26556edaf24a4c778292b
Anshydv18 <anshgithub@gmail.com>
2024-11-17 23:00:49 +0530

    forms

---
+++ 13WebRequests/Forms.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+type Person struct {
+	Name string
+	Age  int
+	Tags []string
+}
+
+func Data() {
+	data := []Person{
+		{"Ansh", 12, []string{"A", "B"}},
+		{"Dev", 14, []string{"c", "d"}},
+	}
+
+	finaljson, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf(string(finaljson))
+}
+
+func Form() {
+	path := "abc.com"
+	data := url.Values{}
+	data.Add("Name", "Ansh")
+	data.Add("address", "gurugram")
+
+	res, err := http.PostForm(path, data)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	content, _ := ioutil.ReadAll(res.Body)
+	fmt.Printf(string(content))
+}
